c21075c3a79e9956c90d471d7345077a745d8272
Meghan Christensen <meghanch@unc.edu>
2023-11-01 21:55:12 -0400

    ex06

--- exercises 6/ex00_hello_world.py
+++
@@ -1,5 +0,0 @@
-"""My first program for COMP110."""
-
-__author__ = "730659220"
-
-print("Hello big silly world, how are you doing?")
--- lessons/zip.py
+++ lessons/zip.py
@@ -13,3 +13,21 @@ def zip(keys: list[str], val: list[int]) -> dict[str, int]:
     for idx in range(0, len(keys)):
         the_dict[keys[idx]] = val[idx]
     return the_dict
+
+
+
+def favorite_color(fav_color: dict[str, str]) -> str:
+    """Returns most liked color from list of people's preferences!"""
+    fav_color: dict[str, str] = {}
+    color_count = 0
+    pop_color: None
+    for color in fav_color:
+        if color in fav_color:
+            color_count[color] += 1
+            pop_color = color_count
+        else:
+            color_count[color] = 1
+    return pop_color
+
+
+print(favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}))
