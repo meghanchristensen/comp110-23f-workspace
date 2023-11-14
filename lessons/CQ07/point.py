c56143ee04d3cda57f2d54edef87fc7fa8989a85
Meghan Christensen <meghanch@unc.edu>
2023-11-14 13:34:36 -0500

    work recently

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -10,7 +10,7 @@ class Point:
     x: float
     y: float
 
-    def __init__(self, x_init: float, y_init: float):
+    def __init__(self, x_init: float = 0.0, y_init: float = 0.0):
         """My constructor."""
         self.x = x_init
         self.y = y_init
@@ -22,6 +22,23 @@ class Point:
 
     def scale(self, factor: int) -> Point:
         """Method that creates a new Point rather than mutating it."""
-        n_x = self.x * factor
-        n_y = self.y * factor
-        return Point(n_x, n_y)
+        return Point(self.x * factor, self.y * factor)
+    
+    def __str__(self) -> str:
+        """Prints values in proper format!"""
+        output: str = f"x: {self.x}; y: {self.y}"
+        return output
+    
+    def __mul__(self, factor: int | float) -> Point:
+        """Multiplies x and y values by an assigned factor!"""
+        mul_point: Point = Point(self.x, self.y)
+        mul_point.x *= factor
+        mul_point.y *= factor
+        return mul_point
+    
+    def __add__(self, factor: int | float) -> Point:
+        """Adds x and y values by an assigned factor!"""
+        add_point: Point = Point(self.x, self.y)
+        add_point.x += factor
+        add_point.y += factor
+        return add_point
